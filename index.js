d6418915e4896aafcf34b0b1f86ed1b32ab2e75c
ollen <oli4wk@gmail.com>
2021-01-01 18:33:00 +0100

    final commit

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ const port = process.env.PORT || 5000
 app.get('/', async (req, res) => {
   console.log(req.query['hub.challenge']);
   let para = new URLSearchParams();
-  para.append('Body', 'hello from nodejs-fetch with code: '+req.query['hub.challenge']);
+  if req.query['hub.mode'] == "subscribe" {
+  	para.append('Body', 'Got a subscribe reqeust with code: '+req.query['hub.challenge']);
+  } else {
+	para.append('Body', 'Got a othre reqesut with the body: '+req.body);
+  }
   para.append('From', "+14157413728");
   para.append('To', "+31622339914");
   let response = await fetch('https://api.twilio.com/2010-04-01/Accounts/ACd0ee4eaae06ee358c0c3f4a7e0160382/Messages.json', {
     method: 'POST',
     body: para,
     headers: {
-     'Authorization': 'Basic QUNkMGVlNGVhYWUwNmVlMzU4YzBjM2Y0YTdlMDE2MDM4MjoxMTYwZmE5ODE4M2VmZDM4YTA3YjUxYTg1NTIyN2UxZA=='
+     'Authorization': 'Basic '+process.env.bs64-Twilio
      }
   });
   console.log(response.status);
