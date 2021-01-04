560859851699cb3f0218352f10efcddbb405f49c
ollen <oli4.games@hotmail.com>
2021-01-04 18:23:04 +0100

    One of the final commit's at 01-04-2021;18:23

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const { URLSearchParams } = require('url');
 const app = express();
 const port = process.env.PORT;
 app.use(bodyParser.text({type:"*/*"}));
+const options = {
+    attributeNamePrefix : "@_",
+    attrNodeName: "attr", //default is 'false'
+    textNodeName : "#text",
+    ignoreAttributes : false,
+    ignoreNameSpace : false,
+    allowBooleanAttributes : false,
+    parseNodeValue : true,
+    parseAttributeValue : true,
+    trimValues: true,
+    parseTrueNumberOnly: false,
+    arrayMode: false, //"strict"
+};
 
 app.all('/', async (req, res) => {
   console.log(req.query['hub.challenge']);
@@ -14,9 +27,18 @@ app.all('/', async (req, res) => {
   } else if (req.query['hub.mode'] == "unsubscribe") {
     	para.append('Body', 'Got a unsubscribe request with code: '+req.query['hub.challenge']+" and with the topic: "+req.query['hub.topic'].split("channel_id")[1]);
   } else {
-    	para.append('Body', 'Got another request with the body: '+JSON.stringify(req.body));
+    //	para.append('Body', 'Got another request with the body: '+JSON.stringify(req.body));
     	console.log('Got another request with the body: '+JSON.stringify(req.body))
-  }
+       if( parser.validate(xmlData) === true) { //optional (it'll return an object in case it's not valid)
+         var tObj = parser.getTraversalObj(xmlData,options);
+         var jsonObj = parser.convertToJson(tObj,options);
+         let video = jsonObj.feed.entry
+         para.append("Er is een nieuwe video van: "+video.author.name+" met de titel "+video.title+" en de link "+video.link.attr["@_href"]+".");
+
+       } else {
+         para.append('Body', 'Er ging iets mis met de xmldata dit is het request met de body: '+JSON.stringify(req.body));
+       }
+    }
   para.append('From', "whatsapp:+14155238886");
   para.append('To', "whatsapp:+31622339914");
   let response = await fetch('https://api.twilio.com/2010-04-01/Accounts/AC560f51cedc776774bd44d9c9688fbc43/Messages.json', {
