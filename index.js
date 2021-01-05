28f8feeae021ba59cdbb4918966b0edac93acd13
ollen <oli4.games@hotmail.com>
2021-01-05 14:06:04 +0100

    One of the final commit's at 01-05-2021;14:06

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const express = require('express');
 var fetch = require('node-fetch');
-var bodyParser = require('body-parser');
-require('body-parser-xml')(bodyParser);
+var xmlparser = require('express-xml-bodyparser');
 const { URLSearchParams } = require('url');
 const app = express();
 const port = process.env.PORT;
-app.use(bodyParser.xml({
-  limit: '1MB',   // Reject payload bigger than 1 MB
-  xmlParseOptions: {
-    normalize: true,     // Trim whitespace inside text nodes
-    normalizeTags: true, // Transform tags to lowercase
-    explicitArray: false // Only put nodes in array if >1
-  }
-}));
+const xml2jsDefaults = {
+    explicitArray: false,
+    normalize: false,
+    normalizeTags: false,
+    trim: true
+}
+app.use(xmlparser(xml2jsDefaults));
 
 app.all('/', async (req, res) => {
-  console.log(req.query['hub.challenge']);
+  console.log("challenge: "+req.query['hub.challenge']);
   let para = new URLSearchParams();
   if (req.query['hub.mode'] == "subscribe") {
   	para.append('Body', 'Got a subscribe request with code: '+req.query['hub.challenge']+" and with the channel: https://www.youtube.com/channel/"+req.query['hub.topic'].split("channel_id")[1]);
   } else if (req.query['hub.mode'] == "unsubscribe") {
     	para.append('Body', 'Got a unsubscribe request with code: '+req.query['hub.challenge']+" and with the topic: "+req.query['hub.topic'].split("channel_id")[1]);
   } else {
-    	para.append('Body', 'Got another request with the body: '+JSON.stringify(req.body));
-    	console.log('Got another request with the body: '+JSON.stringify(req.body)+"\n\n");
-      /*console.log("Er is een nieuwe video van: "+video.author.name+" met de titel "+video.title+" en de link "+video.link.attr["@_href"]+".");
-      para.append("Er is een nieuwe video van: "+video.author.name+" met de titel "+video.title+" en de link "+video.link.attr["@_href"]+".");*/
+    	/*para.append('Body', 'Got another request with the body: '+JSON.stringify(req.body.feed));
+    	console.log('Got another request with the body: '+JSON.stringify(req.body));*/
+      let video = req.body.feed.entry
+      para.append("Body", "Er is een nieuwe video van "+video.author.name+" met de titel ```'"+video.title+"'.```\n "+video.link["$"].href+" .");
+      console.log("Er is een nieuwe video van "+video.author.name+" met de titel: "+video.title+" . "+video.link["$"].href+" .");
     }
   para.append('From', "whatsapp:+14155238886");
   para.append('To', "whatsapp:+31622339914");
